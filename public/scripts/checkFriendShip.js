7960b3d0721574ee99e54d9e2d884adfd08ad356
Sai Darshan <saidarshan27@gmail.com>
2020-05-24 21:27:09 +0530

    checking ownership error

--- public/scripts/checkFriendShip.js
+++ public/scripts/checkFriendShip.js
@@ -1,6 +1,5 @@
 const friendship = document.querySelector(".friendship .data");
 
-
 if(loggedUserId === dataObj.persondata.steamid){
    friendship.classList.remove("mute");
    friendship.classList.add("green");

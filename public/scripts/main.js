025679cc5dff00c9f19ba2c481fdfefab2836237
Sai Darshan <saidarshan27@gmail.com>
2020-06-03 23:10:53 +0530

    add clear input and add ripple effect

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -16,34 +16,41 @@ $(function () {
 }) 
 
 
+
 // Event Listeners
 
+// copy to clipboard button
 $(".copy").click(function(event){
-	// const data = event.target.parentElement.closest(".data-label").childNodes[3];
   const data = event.target.parentElement.closest(".data-label").childNodes[1];
 	const copyBtn = $(this).parents(".data-label").children(".copy");
   copyToClipboard(data);
   $(this).attr('data-original-title', 'Copied!').tooltip('show');
 	$(this).delay(800).attr("data-original-title","Copy to Clipboard");
-	copyBtn.addClass("copy-animation");
-	setTimeout(function(){
-		copyBtn.removeClass("copy-animation");
-		$(this).tooltip("hide");
-	},150)
+	// copyBtn.addClass("copy-animation");
+	// setTimeout(function(){
+	// 	copyBtn.removeClass("copy-animation");
+	// 	$(this).tooltip("hide");
+	// },150)
+	let x = event.clientX - event.target.offsetLeft;
+	let y = event.clientY - event.target.offsetTop;
+	const element = $(this);
+	rippleEffect(element,x,y);
 })
 
+// My profile view-more button
 $(".view-more").click(function(event){
 	const viewMoreBtn = $(this);
   viewMoreBtn.siblings(".view-more-links").toggle();
 })
 
-
+// dont show again local storage 
 if(localStorageAvailable() && localStorage.DoNotShowMessageAgain != "true"){
 	$(".send-message-btn").click(function(){
 		$("#send-message-modal").modal("show");
 	})
 }
 
+// dont show again checkbox 
 $(".dont-show-again-checkbox").change(function(event){
 	if($(this).is(":checked")){
 		$(".send-message").html(`
@@ -60,10 +67,12 @@ $(".dont-show-again-checkbox").change(function(event){
 	}
 })
 
+// hiding message modal after send message ok 
 $(".send-message-ok").click(function(){
 	$('#send-message-modal').modal('hide');
 })
 
+// showing added friend and closing after a second 
 $(".add-friend").click(function(){
 	$("#add-friend-modal").modal("show");
 	setTimeout(function(){
@@ -71,6 +80,7 @@ $(".add-friend").click(function(){
 	},1000)
 })
 
+// mobile steam quick links view more 
 $(".steam-links-view-more").click(function(event){
 	if(event.target.parentElement.classList.contains("open")){
 		$(this).html(`<i class="fas fa-chevron-right ml-2 view-more-btn"></i>`);
@@ -83,6 +93,34 @@ $(".steam-links-view-more").click(function(event){
 	}
 })
 
+
+// adding shadow to parent (".controls") when input field is focused
+$(".url-input").focus(function(event){
+	$(this).parents(".controls").css("box-shadow","0px 4px 4px rgba(0, 0, 0, 0.25)")
+})
+$(".url-input").focusout(function(event){
+	$(this).parents(".controls").css("box-shadow","none");
+})
+
+// check field for any characters.If present show the clear-input button
+$(".url-input").on("keydown input cut",function(event){
+	setTimeout(function(){
+		if($(event.target).val() != ""){
+		 $(".clear-input").show();
+		}else{
+		 $(".clear-input").hide();
+		}
+	},100)
+})
+
+// clears the input on click 
+$(".clear-input").click(function(event){
+	$(this).siblings(".url-input").val("");
+	$(this).hide();
+})
+
+
+// close the my profile view more dropdown when clicked else where on the document
 $(document).click(function(event){
 	const objEvent = $(event.target);
 	if(objEvent.hasClass("view-more-btn")){
@@ -111,10 +149,22 @@ function copyToClipboard(data) {
 	document.execCommand('copy');
 	document.body.removeChild(el);
 }
+
 function localStorageAvailable(){
 	if(typeof(Storage) !== "undefined"){
 		return true
 	}else{
 		return false;
 	}
+}
+
+function rippleEffect(element,x,y){
+	let ripples = document.createElement("span");
+	ripples.style.left = x + "px";
+	ripples.style.top = y + "px";
+	ripples.classList.add("ripple");
+	element.append(ripples);
+	setTimeout(()=>{
+    ripples.remove();
+	},1000);
 }

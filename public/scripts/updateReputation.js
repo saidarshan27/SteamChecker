be608d43c248572239d3f807fb742678ea9025d5
Sai Darshan <saidarshan27@gmail.com>
2020-06-13 15:49:35 +0530

    hover only on non touch

--- public/scripts/updateReputation.js
+++ public/scripts/updateReputation.js
@@ -1,10 +1,11 @@
 const reputation = document.querySelector(".reputation .data");
 const svg = $(".reputation .data .parent-svg").children("use");
-const name = document.querySelector(".name-level-wrapper .name-level a");
+const steamrepATitle = document.querySelector(".icon-links a");
 
 if(reputation.innerText.includes("VALVE EMPLOYEE")){
     reputation.style.color = "var(--valve-employee)";
     svg.attr("href","#tick-shield");
+    steamrepATitle.setAttribute("title",`SteamRep | ${dataObj.persondata.personaname}[Valve Employee]  | ${dataObj.steamrepReputation.steamrepurl}`)
 }else if(reputation.innerText.includes("SteamRep Admin")){
   reputation.innerText = "";
   reputation.innerHTML = 
@@ -15,6 +16,7 @@ if(reputation.innerText.includes("VALVE EMPLOYEE")){
    <span class="steamrep-steam">Steam</span><span class="steamrep-rep">Rep</span> Admin
    </span>
   `
+  steamrepATitle.setAttribute("title",`SteamRep | ${dataObj.persondata.personaname}[SteamRep Admin]  | ${dataObj.steamrepReputation.steamrepurl}`)
 }else if(reputation.innerText.includes("SCAMMER")){
   reputation.style.color="red";
   svg.attr("href","#wrong-shield");
